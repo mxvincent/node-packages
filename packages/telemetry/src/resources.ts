460bf28995e15debf2a56038296eb12cef302a73
Maxime Vincent <maxime.vincent@helloaria.eu>
2025-01-10 18:07:44 +0100

    chore: upgrade swc

--- packages/telemetry/src/resources.ts
+++ packages/telemetry/src/resources.ts
@@ -1,51 +1,44 @@
 import { containerDetector } from '@opentelemetry/resource-detector-container'
-import { gcpDetector } from '@opentelemetry/resource-detector-gcp'
 import {
-  Detector,
-  DetectorSync,
-  envDetectorSync,
-  hostDetectorSync,
-  IResource,
-  processDetectorSync,
-  Resource,
-  ResourceDetectionConfig
+	Detector,
+	DetectorSync,
+	envDetectorSync,
+	hostDetectorSync,
+	IResource,
+	processDetectorSync,
+	Resource,
+	ResourceDetectionConfig
 } from '@opentelemetry/resources'
-import {
-  SEMRESATTRS_SERVICE_NAME,
-  SEMRESATTRS_SERVICE_NAMESPACE,
-  SEMRESATTRS_SERVICE_VERSION
-} from '@opentelemetry/semantic-conventions'
+import { ATTR_SERVICE_NAME, ATTR_SERVICE_VERSION } from '@opentelemetry/semantic-conventions'
+import { ATTR_SERVICE_NAMESPACE } from '@opentelemetry/semantic-conventions/incubating'
 import { TelemetryConfig } from './config/schemas'
 
 const awaitAttributes = (detector: DetectorSync): Detector => {
-  return {
-    async detect(config?: ResourceDetectionConfig): Promise<IResource> {
-      const resource = detector.detect(config)
-      await resource.waitForAsyncAttributes?.()
-      return resource
-    }
-  }
+	return {
+		async detect(config?: ResourceDetectionConfig): Promise<IResource> {
+			const resource = detector.detect(config)
+			await resource.waitForAsyncAttributes?.()
+			return resource
+		}
+	}
 }
 
 export const getResourceDetectors = (config: TelemetryConfig): Detector[] => {
-  const detectors = [
-    awaitAttributes(envDetectorSync),
-    awaitAttributes(processDetectorSync),
-    awaitAttributes(hostDetectorSync)
-  ]
-  if (config.detectors.container) {
-    detectors.push(containerDetector)
-  }
-  if (config.detectors.gcp) {
-    detectors.push(gcpDetector)
-  }
-  return detectors
+	const detectors = [
+		awaitAttributes(envDetectorSync),
+		awaitAttributes(processDetectorSync),
+		awaitAttributes(hostDetectorSync)
+	]
+	if (config.detectors.container) {
+		detectors.push(containerDetector as never as Detector)
+	}
+	return detectors
 }
 
 export const getResource = ({ service }: TelemetryConfig) => {
-  return new Resource({
-    [SEMRESATTRS_SERVICE_NAMESPACE]: service.namespace,
-    [SEMRESATTRS_SERVICE_NAME]: service.name,
-    [SEMRESATTRS_SERVICE_VERSION]: service.version
-  })
+	return new Resource({
+		[ATTR_SERVICE_NAMESPACE]: service.namespace,
+		[ATTR_SERVICE_NAME]: service.name,
+		[ATTR_SERVICE_VERSION]: service.version
+	})
 }
--- packages/telemetry/src/sdk/tracer.ts
+++ packages/telemetry/src/sdk/tracer.ts
@@ -1,13 +1,11 @@
-import { TraceExporter } from '@google-cloud/opentelemetry-cloud-trace-exporter'
 import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-http'
 import { SpanExporter } from '@opentelemetry/sdk-trace-node'
 import { TelemetryConfig } from '../config/schemas'
 
 export const getTraceExporter = (config: TelemetryConfig): SpanExporter => {
-  switch (config.exporter.type) {
-    case 'GCP':
-      return new TraceExporter()
-    case 'HTTP':
-      return new OTLPTraceExporter()
-  }
+	switch (config.exporter.type) {
+		case 'HTTP':
+		default:
+			return new OTLPTraceExporter()
+	}
 }
